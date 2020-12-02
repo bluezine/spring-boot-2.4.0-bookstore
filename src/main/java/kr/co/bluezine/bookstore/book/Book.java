6e7b962ace4c31dd31264e473e0f8a48a1efa4d8
Kisig Ian Seo <Kisig Ian Seo@BLUEZINE-ION>
2020-12-03 00:52:28 +0900

    Book entity table structure, add publish date

--- src/main/java/kr/co/bluezine/bookstore/book/Book.java
+++ src/main/java/kr/co/bluezine/bookstore/book/Book.java
@@ -1,5 +1,7 @@
 package kr.co.bluezine.bookstore.book;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,21 @@ import javax.persistence.Id;
  * 
  * @author Kisig Ian Seo
  *
+ *         <pre>
+CREATE TABLE `book` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`title` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`status` INT(6) NOT NULL DEFAULT '0',
+	`auth_name` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`pub_cmpy` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`pub_year` INT(4) NOT NULL,
+	`pub_date` DATETIME NOT NULL,
+	`isbn` VARCHAR(13) NOT NULL COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`id`) USING BTREE
+)
+COMMENT='book item'
+COLLATE='utf8mb4_general_ci';
+ *         </pre>
  */
 @Entity
 public class Book {
@@ -53,7 +70,13 @@ public class Book {
     private String pubYear;
 
     /**
-     * ISBN Code
+     * Publish date
+     */
+    @Column(name = "pub_date")
+    private Date pubDate;
+
+    /**
+     * ISBN code
      */
     @Column(name = "isbn")
     private String isbn;
@@ -104,6 +127,14 @@ public class Book {
 
     public void setPubYear(String pubYear) {
 	this.pubYear = pubYear;
+    }
+
+    public Date getPubDate() {
+	return pubDate;
+    }
+
+    public void setPubDate(Date pubDate) {
+	this.pubDate = pubDate;
     }
 
     public String getIsbn() {
