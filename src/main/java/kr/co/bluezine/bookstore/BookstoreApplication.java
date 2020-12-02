7dc45caaa32366212ff348441b98187a51d15b33
bluezine-note <bluezine0930@gmail.com>
2020-12-02 09:10:09 +0900

    eclipse project file

--- src/main/java/kr/co/bluezine/bookstore/BookstoreApplication.java
+++ src/main/java/kr/co/bluezine/bookstore/BookstoreApplication.java
@@ -2,8 +2,10 @@ package kr.co.bluezine.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "kr.co.bluezine.bookstore" })
 public class BookstoreApplication {
 
     public static void main(String[] args) {
