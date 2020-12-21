04b1cd7fd50025702561e6da2572ff00ef7f62c0
bluezine-note <bluezine0930@gmail.com>
2020-12-21 17:52:35 +0900

    temp

--- src/main/java/kr/co/bluezine/bookstore/book/BookController.java
+++ src/main/java/kr/co/bluezine/bookstore/book/BookController.java
@@ -1,5 +1,7 @@
 package kr.co.bluezine.bookstore.book;
 
+import java.sql.SQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.ColumnMapRowMapper;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.co.bluezine.bookstore.sql.PageEntity;
@@ -31,29 +34,62 @@ public class BookController {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    @RequestMapping(value = "list/{page}/{count}", method = RequestMethod.GET)
-    public PageEntity list(@PathVariable int page, @PathVariable int count) {
+    /**
+     * Get book list
+     * 
+     * @param page
+     * @param count
+     * @param search
+     * @return
+     * @throws NumberFormatException
+     * @throws SQLException
+     */
+    @RequestMapping(value = "list", method = RequestMethod.GET)
+    public PageEntity list(@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "10") int count,
+	    @RequestParam(defaultValue = "") String search) throws NumberFormatException, SQLException {
 	QueryMake make = new QueryMake();
 	make.select("*");
 	make.from("book b");
+	make.appendSearch(search, Book.SEARCH);
 	make.order("id");
 
 	PageEntity entity = new PageEntity();
-	entity.setTotalCount((Long) jdbcTemplate.query(make.countQuery(), new ColumnMapRowMapper()).get(0).get("cnt"));
-	entity.setArrays(jdbcTemplate.query(make.pageQuery(page, count), new BeanPropertyRowMapper<Book>(Book.class)));
+	entity.setTotalCount((Long) jdbcTemplate
+		.query(make.countQuery(), make.preparedStatementSetter, new ColumnMapRowMapper()).get(0).get("cnt"));
+	make.resetIndex();
+	entity.setArrays(jdbcTemplate.query(make.pageQuery(page, count), make.preparedStatementSetter,
+		new BeanPropertyRowMapper<Book>(Book.class)));
 	return entity;
     }
 
+    /**
+     * Read book
+     * 
+     * @param id
+     * @return
+     */
     @RequestMapping(value = "read/{id}", method = RequestMethod.GET)
     public Book read(@PathVariable Long id) {
 	return bookService.load(id);
     }
 
+    /**
+     * Insert book
+     * 
+     * @param item
+     * @return
+     */
     @RequestMapping(value = "insert", method = RequestMethod.POST)
     public Book insert(@RequestBody Book item) {
 	return bookService.save(item);
     }
 
+    /**
+     * Update book
+     * 
+     * @param item
+     * @return
+     */
     @RequestMapping(value = "update", method = RequestMethod.POST)
     public Book update(@RequestBody Book item) {
 	return bookService.save(item);
