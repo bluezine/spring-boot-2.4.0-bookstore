e22171af89b96c8190173908566f7c5a97941475
bluezine-note <bluezine0930@gmail.com>
2020-12-21 17:50:40 +0900

    Book code

--- src/main/java/kr/co/bluezine/bookstore/book/BookCode.java
+++ src/main/java/kr/co/bluezine/bookstore/book/BookCode.java
@@ -2,10 +2,14 @@ package kr.co.bluezine.bookstore.book;
 
 /**
  * Book Code
+ * 
  * @author Kisig Ian Seo
  *
  */
 public class BookCode {
 
+    /**
+     * Normal Status
+     */
     public static final int BOOK_STATUS_NORMAL = 0;
 }
