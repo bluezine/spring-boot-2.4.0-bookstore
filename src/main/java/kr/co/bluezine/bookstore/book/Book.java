598a9ef29b2b05e20c2e6ff6ce9441d666df5710
bluezine-ion <bluezine0930@gmail.com>
2021-04-13 22:41:27 +0900

    Modify column name

--- src/main/java/kr/co/bluezine/bookstore/book/Book.java
+++ src/main/java/kr/co/bluezine/bookstore/book/Book.java
@@ -21,14 +21,14 @@ import kr.co.bluezine.bookstore.sql.SuperEntity;
  *         <pre>
 CREATE TABLE `book` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`title` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`title` VARCHAR(100) NOT NULL,
 	`status` INT(6) NOT NULL DEFAULT '0',
-	`auth_name` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`pub_cmpy` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`auth_name` VARCHAR(100) NOT NULL,
+	`pub_cmpy` VARCHAR(100) NOT NULL,
 	`pub_year` INT(4) NOT NULL,
 	`pub_date` DATETIME NOT NULL,
-	`isbn` VARCHAR(13) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`desc` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	`isbn` VARCHAR(13) NOT NULL,
+	`description` LONGTEXT NULL DEFAULT NULL,
 	PRIMARY KEY (`id`) USING BTREE
 )
 COMMENT='book item'
@@ -39,141 +39,141 @@ ENGINE=InnoDB
  */
 @Entity(name = "book")
 public class Book extends SuperEntity {
-    static {
-	SEARCH.put("id", new SearchParam("id", Types.NUMERIC));
-	SEARCH.put("title", new SearchParam("title", Types.VARCHAR, SearchType.SEARCH_LIKE));
-	SEARCH.put("status", new SearchParam("status", Types.NUMERIC));
-	SEARCH.put("authorName", new SearchParam("auth_name", Types.VARCHAR, SearchType.SEARCH_LIKE));
-	SEARCH.put("pubCompany", new SearchParam("pub_cmpy", Types.VARCHAR, SearchType.SEARCH_LIKE));
-	SEARCH.put("pubYear", new SearchParam("pub_year", Types.NUMERIC));
-	SEARCH.put("pubDate", new SearchParam("pubDate", Types.DATE));
-	SEARCH.put("isbn", new SearchParam("isbn", Types.VARCHAR, SearchType.SEARCH_LIKE));
-    }
+	static {
+		SEARCH.put("id", new SearchParam("id", Types.NUMERIC));
+		SEARCH.put("title", new SearchParam("title", Types.VARCHAR, SearchType.SEARCH_LIKE));
+		SEARCH.put("status", new SearchParam("status", Types.NUMERIC));
+		SEARCH.put("authorName", new SearchParam("auth_name", Types.VARCHAR, SearchType.SEARCH_LIKE));
+		SEARCH.put("pubCompany", new SearchParam("pub_cmpy", Types.VARCHAR, SearchType.SEARCH_LIKE));
+		SEARCH.put("pubYear", new SearchParam("pub_year", Types.NUMERIC));
+		SEARCH.put("pubDate", new SearchParam("pubDate", Types.DATE));
+		SEARCH.put("isbn", new SearchParam("isbn", Types.VARCHAR, SearchType.SEARCH_LIKE));
+	}
 
-    /**
-     * Identify Key (PK)
-     */
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	/**
+	 * Identify Key (PK)
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    /**
-     * Book Title
-     */
-    @Column(name = "title")
-    private String title;
+	/**
+	 * Book Title
+	 */
+	@Column(name = "title")
+	private String title;
 
-    /**
-     * Book Status
-     */
-    @Column(name = "status")
-    private int status;
+	/**
+	 * Book Status
+	 */
+	@Column(name = "status")
+	private int status;
 
-    /**
-     * Author Name
-     */
-    @Column(name = "auth_name")
-    private String authorName;
+	/**
+	 * Author Name
+	 */
+	@Column(name = "auth_name")
+	private String authorName;
 
-    /**
-     * Publish Company Name
-     */
-    @Column(name = "pub_cmpy")
-    private String pubCompany;
+	/**
+	 * Publish Company Name
+	 */
+	@Column(name = "pub_cmpy")
+	private String pubCompany;
 
-    /**
-     * Publish Year
-     */
-    @Column(name = "pub_year")
-    private int pubYear;
+	/**
+	 * Publish Year
+	 */
+	@Column(name = "pub_year")
+	private int pubYear;
 
-    /**
-     * Publish Date
-     */
-    @Column(name = "pub_date")
-    private Date pubDate;
+	/**
+	 * Publish Date
+	 */
+	@Column(name = "pub_date")
+	private Date pubDate;
 
-    /**
-     * ISBN Code
-     */
-    @Column(name = "isbn")
-    private String isbn;
+	/**
+	 * ISBN Code
+	 */
+	@Column(name = "isbn")
+	private String isbn;
 
-    /**
-     * Description
-     */
-    @Column(name = "desc")
-    private String desc;
+	/**
+	 * Description
+	 */
+	@Column(name = "description")
+	private String desc;
 
-    public Long getId() {
-	return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-	this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getTitle() {
-	return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public void setTitle(String title) {
-	this.title = title;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public int getStatus() {
-	return status;
-    }
+	public int getStatus() {
+		return status;
+	}
 
-    public void setStatus(int status) {
-	this.status = status;
-    }
+	public void setStatus(int status) {
+		this.status = status;
+	}
 
-    public String getAuthorName() {
-	return authorName;
-    }
+	public String getAuthorName() {
+		return authorName;
+	}
 
-    public void setAuthorName(String authorName) {
-	this.authorName = authorName;
-    }
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
 
-    public String getPubCompany() {
-	return pubCompany;
-    }
+	public String getPubCompany() {
+		return pubCompany;
+	}
 
-    public void setPubCompany(String pubCompany) {
-	this.pubCompany = pubCompany;
-    }
+	public void setPubCompany(String pubCompany) {
+		this.pubCompany = pubCompany;
+	}
 
-    public int getPubYear() {
-	return pubYear;
-    }
+	public int getPubYear() {
+		return pubYear;
+	}
 
-    public void setPubYear(int pubYear) {
-	this.pubYear = pubYear;
-    }
+	public void setPubYear(int pubYear) {
+		this.pubYear = pubYear;
+	}
 
-    public Date getPubDate() {
-	return pubDate;
-    }
+	public Date getPubDate() {
+		return pubDate;
+	}
 
-    public void setPubDate(Date pubDate) {
-	this.pubDate = pubDate;
-    }
+	public void setPubDate(Date pubDate) {
+		this.pubDate = pubDate;
+	}
 
-    public String getIsbn() {
-	return isbn;
-    }
+	public String getIsbn() {
+		return isbn;
+	}
 
-    public void setIsbn(String isbn) {
-	this.isbn = isbn;
-    }
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
 
-    public String getDesc() {
-	return desc;
-    }
+	public String getDesc() {
+		return desc;
+	}
 
-    public void setDesc(String desc) {
-	this.desc = desc;
-    }
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
 }
