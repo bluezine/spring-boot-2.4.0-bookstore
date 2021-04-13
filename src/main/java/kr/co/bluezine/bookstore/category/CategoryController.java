fdd2010f433628ec219c8100289bdf37d06fbd5a
bluezine-ion <bluezine0930@gmail.com>
2021-04-13 23:09:27 +0900

    Swagger apply

--- src/main/java/kr/co/bluezine/bookstore/category/CategoryController.java
+++ src/main/java/kr/co/bluezine/bookstore/category/CategoryController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
 import kr.co.bluezine.bookstore.sql.PageEntity;
 
 /**
@@ -25,101 +26,109 @@ import kr.co.bluezine.bookstore.sql.PageEntity;
 @RequestMapping(value = "category")
 public class CategoryController {
 
-    @Autowired
-    private CategoryService categoryService;
+	@Autowired
+	private CategoryService categoryService;
 
-    /**
-     * Get Category List
-     * 
-     * @param id
-     * @return
-     */
-    @RequestMapping(value = "list", method = RequestMethod.GET)
-    public PageEntity list() {
-	return categoryService.rootList();
-    }
-
-    /**
-     * Get Category List
-     * 
-     * @param id
-     * @return
-     */
-    @RequestMapping(value = "list/{id}", method = RequestMethod.GET)
-    public PageEntity list(@PathVariable Long id) {
-	return categoryService.childrenList(id);
-    }
-
-    /**
-     * Read Category
-     * 
-     * @param id
-     * @return
-     */
-    @RequestMapping(value = "read/{id}", method = RequestMethod.GET)
-    public Category read(@PathVariable Long id) {
-	return categoryService.load(id);
-    }
-
-    /**
-     * Insert Category
-     * 
-     * @param item
-     * @return
-     */
-    @RequestMapping(value = "insert", method = RequestMethod.POST)
-    public Category insert(@RequestBody Category item) {
-	return categoryService.create(item);
-    }
-
-    /**
-     * Update Category
-     * 
-     * @param item
-     * @return
-     */
-    @RequestMapping(value = "update", method = RequestMethod.POST)
-    public Category update(@RequestBody Category item) {
-	return categoryService.save(item);
-    }
-
-    /**
-     * Delete Category
-     * 
-     * @param item
-     * @return
-     */
-    @RequestMapping(value = "delete", method = RequestMethod.POST)
-    public Category delete(@RequestBody Category item) {
-	return categoryService.delete(item);
-    }
-
-    /**
-     * Recover Category
-     * 
-     * @param item
-     * @return
-     */
-    @RequestMapping(value = "restore", method = RequestMethod.POST)
-    public Category recover(@RequestBody Category item) {
-	return categoryService.restore(item);
-    }
-
-    /**
-     * Swap Category
-     * 
-     * @param items
-     * @return
-     */
-    @RequestMapping(value = "swap", method = RequestMethod.POST)
-    public Map<String, Boolean> swap(@RequestBody List<Long> ids) {
-	Map<String, Boolean> result = new HashMap<>();
-	try {
-	    categoryService.swap(ids.get(0), ids.get(1));
-	    result.put("result", true);
-	} catch (Exception ex) {
-	    result.put("result", false);
+	/**
+	 * Get Category List
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "list", method = RequestMethod.GET)
+	@ApiOperation(value = "Category List")
+	public PageEntity list() {
+		return categoryService.rootList();
 	}
-	return result;
-    }
+
+	/**
+	 * Get Category List
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "list/{id}", method = RequestMethod.GET)
+	@ApiOperation(value = "Category Children List")
+	public PageEntity list(@PathVariable Long id) {
+		return categoryService.childrenList(id);
+	}
+
+	/**
+	 * Read Category
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "read/{id}", method = RequestMethod.GET)
+	@ApiOperation(value = "Category Read")
+	public Category read(@PathVariable Long id) {
+		return categoryService.load(id);
+	}
+
+	/**
+	 * Insert Category
+	 * 
+	 * @param item
+	 * @return
+	 */
+	@RequestMapping(value = "insert", method = RequestMethod.POST)
+	@ApiOperation(value = "Category Insert")
+	public Category insert(@RequestBody Category item) {
+		return categoryService.create(item);
+	}
+
+	/**
+	 * Update Category
+	 * 
+	 * @param item
+	 * @return
+	 */
+	@RequestMapping(value = "update", method = RequestMethod.POST)
+	@ApiOperation(value = "Category Update")
+	public Category update(@RequestBody Category item) {
+		return categoryService.save(item);
+	}
+
+	/**
+	 * Delete Category
+	 * 
+	 * @param item
+	 * @return
+	 */
+	@RequestMapping(value = "delete", method = RequestMethod.POST)
+	@ApiOperation(value = "Category Delete")
+	public Category delete(@RequestBody Category item) {
+		return categoryService.delete(item);
+	}
+
+	/**
+	 * Recover Category
+	 * 
+	 * @param item
+	 * @return
+	 */
+	@RequestMapping(value = "restore", method = RequestMethod.POST)
+	@ApiOperation(value = "Category Restore")
+	public Category recover(@RequestBody Category item) {
+		return categoryService.restore(item);
+	}
+
+	/**
+	 * Swap Category
+	 * 
+	 * @param items
+	 * @return
+	 */
+	@RequestMapping(value = "swap", method = RequestMethod.POST)
+	@ApiOperation(value = "Category Swap")
+	public Map<String, Boolean> swap(@RequestBody List<Long> ids) {
+		Map<String, Boolean> result = new HashMap<>();
+		try {
+			categoryService.swap(ids.get(0), ids.get(1));
+			result.put("result", true);
+		} catch (Exception ex) {
+			result.put("result", false);
+		}
+		return result;
+	}
 }
